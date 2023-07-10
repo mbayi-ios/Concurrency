c020b56fdf0fc538129d26ee819c4c9f0fc2d390
Ambrose Mbayi <91916741+mbayi-ios@users.noreply.github.com>
2023-07-11 00:08:40 +0300

    defining asynchronous properties

--- Concurrency/ContentView.swift
+++ Concurrency/ContentView.swift
@@ -8,30 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    var thumbnail: String {
+        get async {
+            try? await Task.sleep(nanoseconds: 3 * 1000_000_000)
+            return "mythumbail"
+        }
+    }
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .padding()
         }
-        .padding()
+       
         .onAppear{
             Task(priority: .background) {
-                let imageName1 = await loadImage(name: "image1")
-                let imageName2 = await loadImage(name: "image2")
-                let imageName3 = await loadImage(name: "image3")
-                print("\(imageName1), \(imageName2), and \(imageName3)")
+                let imageName = await thumbnail
+                print(imageName)
             }
         }
     }
     
-    func loadImage(name: String) async -> String {
-        try? await Task.sleep(nanoseconds: 3 * 1000_000_000)
-        return "Name: \(name)"
-    }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
