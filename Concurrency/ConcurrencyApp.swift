ae5c897065a3623581d281fb1f054408595f953f
Ambrose Mbayi <91916741+mbayi-ios@users.noreply.github.com>
2023-07-10 23:34:42 +0300

    Initial Commit

---
+++ Concurrency/ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyApp.swift
+//  Concurrency
+//
+//  Created by Ambrose Mbayi on 10/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
