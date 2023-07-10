0aa2783b1fb34632c3fa8ec814e6ab7844f1de9d
Ambrose Mbayi <91916741+mbayi-ios@users.noreply.github.com>
2023-07-10 23:58:49 +0300

    defining a task explicitly

--- Concurrency/ContentView.swift
+++ Concurrency/ContentView.swift
@@ -16,11 +16,13 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
-        .task(priority: .background) {
-            let imageName1 = await loadImage(name: "image1")
-            let imageName2 = await loadImage(name: "image2")
-            let imageName3 = await loadImage(name: "image3")
-            print("\(imageName1), \(imageName2), and \(imageName3)")
+        .onAppear{
+            Task(priority: .background) {
+                let imageName1 = await loadImage(name: "image1")
+                let imageName2 = await loadImage(name: "image2")
+                let imageName3 = await loadImage(name: "image3")
+                print("\(imageName1), \(imageName2), and \(imageName3)")
+            }
         }
     }
     
